f42d861042e9a8212b895c1c93360783e6c7468c
aommiez <aommiez@gmail.com>
2013-09-09 12:07:43 +0700

    fix menu link

--- home.php
+++ home.php
@@ -41,9 +41,9 @@ $page = isset($_GET['page'])? $_GET['page']: 'home';
                     <a href="home.php" class="<?php if($page=='home') echo 'active';?>">Home</a>
                     <a href="home.php?page=meeting_info" class="<?php if($page=='meeting_info') echo 'active';?>">Meeting Info</a>
                     <a href="home.php?page=agenda" class="<?php if($page=='agenda') echo 'active';?>">Agenda</a>
-                    <a href="home.php?page=explore_thailand" class="<?php if($page=='explore_thailand') echo 'active';?>">Explore Thailand</a>
-                    <a href="home.php?page=who_is_coming" class="<?php if($page=='who_is_coming') echo 'active';?>">Who's coming?</a>
-                    <a href="home.php?page=gallery" class="<?php if($page=='gallery') echo 'active';?>">Gallery</a>
+                    <a href="#home.php?page=explore_thailand" class="<?php if($page=='explore_thailand') echo 'active';?>">Explore Thailand</a>
+                    <a href="#home.php?page=who_is_coming" class="<?php if($page=='who_is_coming') echo 'active';?>">Who's coming?</a>
+                    <a href="#home.php?page=gallery" class="<?php if($page=='gallery') echo 'active';?>">Gallery</a>
                 </div>
             </div>
             <div class="clearfix"></div>
